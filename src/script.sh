8feee32f5d7ee6e7313d9be046cde96d20127670
KaPally3 <llwguestwll@gmail.com>
2023-09-15 01:31:02 +0700

    4-5

--- src/script.sh
+++ src/script.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
 date +%D	
 whoami
 hostname
+lscpu | grep -E '^Модель|^Model'
+lscpu | grep -E '^Архитектура|^Architecture'
+sudo  dmidecode -t processor | grep -E 'Current Speed'
+lscpu | grep -m 1 -w "^CPU(s):"
+lscpu | grep -E '^Thread'
+grep -E '^MemTotal|^MemAvailable' /proc/meminfo
