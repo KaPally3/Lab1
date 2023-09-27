8dbbe742b568ec384d860250e007309bb0b6806e
KaPally3 <llwguestwll@gmail.com>
2023-09-28 00:49:23 +0700

    report

--- src/script.sh
+++ src/script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-date +%D	
-whoami
-hostname
-lscpu | grep -E '^Модель|^Model'
-lscpu | grep -E '^Архитектура|^Architecture'
-sudo  dmidecode -t processor | grep -E 'Current Speed'
+echo "Дата: $(date +%D)"	
+echo "Имя учетной записи: $(whoami)"
+echo "Доменное имя ПК: $(hostname)"
+echo -e "\033[36mПроцессор\033[0m"
+echo "Модель: $(lscpu | grep -E '^Имя модели|^Model name' | sed 's/Имя модели:*\s*//;s/Model name:*\s*//')"
+echo "Архитектура: $(lscpu | grep -E '^Архитектура|^Architecture' | sed 's/Архитектура:*\s*//;s/Architecture:*\s*//')"
+echo "Тактовая частота: $(sudo  dmidecode -t processor | grep -E 'Current Speed' | sed 's/\t//;s/Current Speed:*\s*//')" 
+echo "Количество ядер: $( lscpu | grep -m 1 -w "^CPU(s):" | sed 's/CPU(s):*\s*//')"
+
 lscpu | grep -m 1 -w "^CPU(s):"
 lscpu | grep -E '^Thread'
 grep -E '^MemTotal|^MemAvailable' /proc/meminfo
