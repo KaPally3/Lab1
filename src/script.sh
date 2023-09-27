d89e0d50238dafa52129779b8c2bd3a2f2ea7cd0
KaPally3 <llwguestwll@gmail.com>
2023-09-28 01:34:07 +0700

    report

--- src/script.sh
+++ src/script.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
-echo "Дата: $(date +%D)"	
-echo "Имя учетной записи: $(whoami)"
-echo "Доменное имя ПК: $(hostname)"
-echo -e "\033[36mПроцессор\033[0m"
-echo "Модель: $(lscpu | grep -E '^Имя модели|^Model name' | sed 's/Имя модели:*\s*//;s/Model name:*\s*//')"
-echo "Архитектура: $(lscpu | grep -E '^Архитектура|^Architecture' | sed 's/Архитектура:*\s*//;s/Architecture:*\s*//')"
-echo "Тактовая частота: $(sudo  dmidecode -t processor | grep -E 'Current Speed' | sed 's/\t//;s/Current Speed:*\s*//')" 
-echo "Количество ядер: $( lscpu | grep -m 1 -w "^CPU(s):" | sed 's/CPU(s):*\s*//')"
 
-lscpu | grep -m 1 -w "^CPU(s):"
-lscpu | grep -E '^Thread'
-grep -E '^MemTotal|^MemAvailable' /proc/meminfo
+echo "Дата: $(date +%D)"
+	
+echo "Имя учетной записи: $(whoami)"
+
+echo "Доменное имя ПК: $(hostname)"
+
+echo -e "\033[36mПроцессор\033[0m"
+
+echo "Модель: $(lscpu | grep -E '^Имя модели|^Model name' | sed 's/Имя модели:*\s*//;s/Model name:*\s*//')"
+
+echo "Архитектура: $(lscpu | grep -E '^Архитектура|^Architecture' | sed 's/Архитектура:*\s*//;s/Architecture:*\s*//')"
+
+echo "Тактовая частота: $(sudo  dmidecode -t processor | grep -E 'Current Speed' | sed 's/\t//;s/Current Speed:*\s*//')"
+ 
+echo "Количество ядер: $(lscpu | grep -m 1 -w "^CPU(s):" | sed 's/CPU(s):*\s*//')"
+
+echo "Количество потоков на одно ядро: $(lscpu | grep -E '^Thread' | sed 's/Thread(s) per core:\s*//')"
+
+echo -e "\033[32mОперативная память\033[0m"
+
+echo "Всего: $(grep -E '^MemTotal' /proc/meminfo | sed 's/MemTotal:*\s*//')"
+
+echo "Доступно: $(grep -E '^MemAvailable' /proc/meminfo | sed 's/MemAvailable:*\s*//')"
+
